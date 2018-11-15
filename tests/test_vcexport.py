98a07bf5b1f4144a0396ebfb6178a7fbf7c91af1
Tor-Åke Fransson <torake.fransson@digitalroute.com>
2018-11-15 15:26:22 +0100

    DEVOPS-809: improve usability of vcexport
    Corrections for these problems:
    - user arguments is capital letters in help arguments
    - unable to vcexport when target directory does not exist
    - no logs displays of the items being exported
    - Error handling message is not correct when using different password
    - Arguments in help should be display alphabetically
    - Get message that files have been written to directory. However directory is empty

--- tests/test_vcexport.py
+++ tests/test_vcexport.py
@@ -4,6 +4,7 @@ sys.path.append('..')
 from mztools import vcexport
 from unittest.mock import patch, Mock
 from _pytest.monkeypatch import MonkeyPatch
+import pytest
 
 from argparse import Namespace
 
@@ -12,15 +13,42 @@ class TestVcexport(unittest.TestCase):
      self.monkeypatch = MonkeyPatch()
      self.lambdaMock = Mock()
      self.lambdaMock.run_lambda.return_value = {
-        "tarfile": "UGF4SGVhZGVyL2VtcHR5AAAAAAA="
+        "tarfile": "UGF4SGVhZGVyL2VtcHR5AAAAAAA=",
+        "stdout": "OK",
+        "stderr": ""
      }
 
      self.monkeypatch.setattr('mztools.vcexport.run_lambda', lambda a,b: self.lambdaMock.run_lambda())
      self.monkeypatch.setattr('mztools.vcexport.untar_bytes', lambda a,b: True)
 
+    @patch('sys.stderr')
+    @patch('os.listdir')
+    @patch('glob.glob')
+    def test_warning_when_not_all_folders_exported(self, glob, listdir, mock_stderr):
+            args = Namespace(**{
+                "environment": "test",
+                "directory": "mocktmpdir",
+                "overwrite": False,
+                "excludesysdata": False,
+                "includemeta": True,
+                "user": "user/passwd",
+                "folders": [ "Examples", "Defaults" ]
+            })
+
+            listdir.return_value = []
+            glob.return_value = [ "Examples" ]
+
+            result = vcexport.run_vcexport(args)
+
+            self.assertEqual(listdir.called, True)
+            mock_stderr.assert_has_calls([
+                unittest.mock.call.write('Warning: folder "Defaults" was not found in the export' + "\n")
+            ])
+
     @patch('getpass.getpass')
     @patch('os.listdir')
-    def test_asks_for_password_if_not_in_arguments(self, listdir, getpass):
+    @patch('glob.glob')
+    def test_asks_for_password_if_not_in_arguments(self, glob, listdir, getpass):
         args = Namespace(**{
             "environment": "test",
             "directory": ".",
@@ -32,7 +60,8 @@ class TestVcexport(unittest.TestCase):
         })
         listdir.return_value = []
         getpass.return_value = "thepass"
-
+        glob.return_value = [ "Examples" ]
+#        self.monkeypatch.setattr('mztools.vcexport.checkexportok', lambda a,b: True)
 
         result = vcexport.run_vcexport(args)
 
@@ -41,7 +70,8 @@ class TestVcexport(unittest.TestCase):
 
     @patch('getpass.getpass')
     @patch('os.listdir')
-    def test_does_not_ask_for_passwd_if_given(self, listdir, getpass):
+    @patch('glob.glob')
+    def test_does_not_ask_for_passwd_if_given(self, glob, listdir, getpass):
         args = Namespace(**{
             "environment": "test",
             "directory": ".",
@@ -53,6 +83,7 @@ class TestVcexport(unittest.TestCase):
         })
         listdir.return_value = []
         getpass.return_value = "thepass"
+        glob.return_value = [ "Examples" ]
         self.lambdaMock.reset_mock()
 
         result = vcexport.run_vcexport(args)
@@ -79,3 +110,21 @@ class TestVcexport(unittest.TestCase):
 
         self.lambdaMock.run_lambda.assert_not_called()
         self.assertEqual(result,False)
+
+    @patch('os.path.isdir')
+    @patch('os.mkdir')
+    def test_create_exportdir_ifnotexist(self, mkdir, isdir):
+            args = Namespace(**{
+                "environment": "test",
+                "directory": "doesnotexist",
+                "overwrite": False,
+                "excludesysdata": False,
+                "includemeta": True,
+                "user": "user/passwd",
+                "folders": [ "Examples" ]
+            })
+            isdir.return_value = False
+
+            result = vcexport.run_vcexport(args)
+
+            self.assertEqual(mkdir.called, True)
