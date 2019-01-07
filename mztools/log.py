af396e31229c377004fbc78341b766db6e69a835
Tor-Åke Fransson <torake.fransson@digitalroute.com>
2019-01-07 16:06:03 +0100

    DEVOPS-872: fetch platform log from new log group

--- mztools/log.py
+++ mztools/log.py
@@ -16,27 +16,9 @@ def run_log(args):
 
 def get_log(args):
     env = args.environment
-    if args.json:
-        pretty = False
-    else:
-        pretty = True
 
-    logGroup = '/' + env + '/cluster'
+    logEvents = get_log_stream('/' + env + '/platform')
 
-    logEvents = get_log_stream(
-        logGroup,
-        prefix='kubernetes.var.log.containers.platform'
-        )
-
-    fileName = env + '-platform.log'
-    with open(fileName, 'w') as logFile:
-        for line in logEvents:
-            splitLine = line.split(' - ', 1)
-            if pretty:
-                dictLine = json.loads(splitLine[1])
-                print(splitLine[0] + ' : ' + dictLine['log'].rstrip())
-            else:
-                logFile.write(splitLine[1] + '\n')
-
-        if not pretty:
-            print(colored('  Please see log: "' + fileName + '"\n', 'green'))
+    for line in logEvents:
+        splitLine = line.split(' - ', 1)
+        print(splitLine[0] + ' : ' + splitLine[1])
